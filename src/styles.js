37fb4d01f2ce3afa9dbcfcff3b3e155618209cd5
Ha Nguyen <mjboy1412@gmail.com>
2020-05-12 20:44:56 +0700

    add styled component

---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const StyledSideMenu = styled.div`
+  width: 8.33333333%;
+  height: 100%;
+`;
